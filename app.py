22f3729cc3f5a828fd9c73af4921c347fd6eec94
knak <dr.kn.x629@gmail.com>
2025-08-03 03:15:47 +0900

    Created download GUI by Pyside6

--- app.py
+++ app.py
@@ -1,31 +1,140 @@
+import PySide6
+from PySide6.QtWidgets import (QApplication,
+                               QWidget,
+                               QLabel,
+                               QPushButton,
+                               QLineEdit,
+                               QFileDialog)
+
+from PySide6.QtCore import (Signal, 
+                            #Slot,
+                            #QObject
+                            )
+
+import os
+import sys
+
 import argparse
-from pathlib import Path
-import ffmpeg
-from yt_dlp import YoutubeDL
+from dl_mv import download_youtube
 
-def download_youtube(url, output_dir):
-    path_out_dir = Path(output_dir)
-    path_webm = path_out_dir / "out.webm"
-    ydl_opts = {
-        'format': 'bestvideo+bestaudio/best',
-        'outtmpl': str(path_webm)
-    }
-    with YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
+# PySide6のアプリ本体（ユーザがコーディングしていく部分）
+class MainWindow(QWidget):
+    # main
+    def __init__(self, parent=None):
+        # 親クラスの初期化
+        super().__init__(parent)
         
-    path_out_dir = Path(output_dir)
-    path_mp4 = path_out_dir / "out.mp4"
-    path_mp3 = path_out_dir / "out.mp3"
+        # ウィンドウタイトル
+        self.setWindowTitle("ダウンロード")
+        
+        # ウィンドウサイズ
+        win_width = 650
+        win_hegiht = 200
+        self.resize(win_width, win_hegiht)
+        
+        # ラベルの表示
+        self.SetLabel()
+        
+        # ボタンの表示
+        self.SetButoon()
+        
+        # 入力欄の表示
+        self.SetLineedit()
 
-    ffmpeg.input(str(path_webm)).output(str(path_mp4), vcodec="libx264").run()
-    ffmpeg.input(str(path_mp4)).output(str(path_mp3)).run()
+    #　ラベル メソッド
+    def SetLabel(self):
+        label = QLabel(self)
+        label_dir = QLabel(self)
+        
+        # ラベルの見た目をQt Style Sheetで設定
+        labelStyle = """QLabel {
+            color:            #000000;  /* 文字色 */
+            font-size:        16px;     /* 文字サイズ */
+        }"""
+        
+        label.setStyleSheet(labelStyle)
+        label_dir.setStyleSheet(labelStyle)
+        
+        label.setText("動画のURL :")
+        label.move(35, 45)
+        
+        label_dir.setText("保存先 :")
+        label_dir.move(35, 90)
+    
+    # ボタン メソッド
+    def SetButoon(self):
+        button = QPushButton(self)
+        button.move(250, 135)
+        button.setText("ダウンロード")
+        
+        button_dir = QPushButton(self)
+        button_dir.move(500, 90)
+        button_dir.setText("...")
+        
+        # ボタンのクリックによる実行処理
+        button.pressed.connect(lambda: self.CallbackButtonPressed(self.lineEdit.text(), self.lineEdit_dir.text()))
+        button_dir.pressed.connect(self.get_file_path)
+        
+        # ボタンを離した時に、実行される処理
+        # button.released.connect(lambda: self.CallbackButtonReleased(90))  
+    
+    # ボタン　クリック　メソッド
+    def CallbackButtonPressed(self, url, dir):
+        print( url + "が入力されました。")
+        
+        parser = argparse.ArgumentParser(description='YouTubeの動画をダウンロードし、音声を分離します。')
+        parser.add_argument('--url', '-i', type=str, help='YouTubeの動画URL')
+        parser.add_argument('--output-dir', '-o', type=str, default='./movie', help='出力先ディレクトリ')
 
+        args = parser.parse_args()
+        if dir:
+            download_youtube(url, dir)
+        else:
+            download_youtube(url, args.output_dir)
+        
+    #Inputの入力欄　メソッド
+    def SetLineedit(self):
+        self.lineEdit = QLineEdit(self)
+        self.lineEdit.move(150, 45)
+        self.lineEdit.resize(375, 25)
+        
+        self.lineEdit_dir = QLineEdit(self)
+        self.lineEdit_dir.move(150, 90)
+        self.lineEdit_dir.resize(325, 25)
+        
+        self.lineEdit.textChanged.connect(self.CallbaclReturnpressedLineedit)
+        self.lineEdit_dir.textChanged.connect(self.CallbaclReturnpressedLineeditDir)
+    
+    #Inputのテキスト変更時　処理
+    def CallbaclReturnpressedLineedit(self):
+        print("テキストが変更されました。")
+        
+    def CallbaclReturnpressedLineeditDir(self):
+        print("ディレクトリが変更されました。")
 
+    def get_file_path(self):
+        dialog = QFileDialog()
+        dialog.setFileMode(QFileDialog.FileMode.Directory)
+        
+        if dialog.exec():
+            selected_files = dialog.selectedFiles()
+            if selected_files:
+                self.change_text(selected_files[0])
+        return None
+    
+    def change_text(self, text):
+        self.lineEdit_dir.setText(text)
+        
+        
+        
+        
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='YouTubeの動画をダウンロードし、音声を分離します。')
-    parser.add_argument('--url', '-i', type=str, help='YouTubeの動画URL')
-    parser.add_argument('--output-dir', '-o', type=str, default='./movie', help='出力先ディレクトリ')
-
-    args = parser.parse_args()
-
-    download_youtube(args.url, args.output_dir)
+    # 環境変数にPySide6を登録
+    dirname = os.path.dirname(PySide6.__file__)
+    plugin_path = os.path.join(dirname, 'plugins', 'platforms')
+    os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
+    
+    app = QApplication(sys.argv)    # PySide6の実行
+    window = MainWindow()           # ユーザがコーディングしたクラス
+    window.show()                   # PySide6のウィンドウを表示
+    sys.exit(app.exec())            # PySide6の終了
---
+++ dl_mv.py
@@ -0,0 +1,30 @@
+import argparse
+from pathlib import Path
+import ffmpeg
+from yt_dlp import YoutubeDL
+
+def download_youtube(url, output_dir):
+    path_out_dir = Path(output_dir)
+    path_webm = path_out_dir / "out.webm"
+    ydl_opts = {
+        'format': 'bestvideo+bestaudio/best',
+        'outtmpl': str(path_webm)
+    }
+    with YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+        
+    path_out_dir = Path(output_dir)
+    path_mp4 = path_out_dir / "out.mp4"
+    path_mp3 = path_out_dir / "out.mp3"
+
+    ffmpeg.input(str(path_webm)).output(str(path_mp4), vcodec="libx264").run()
+    ffmpeg.input(str(path_mp4)).output(str(path_mp3)).run()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='YouTubeの動画をダウンロードし、音声を分離します。')
+    parser.add_argument('--url', '-i', type=str, help='YouTubeの動画URL')
+    parser.add_argument('--output-dir', '-o', type=str, default='./movie', help='出力先ディレクトリ')
+
+    args = parser.parse_args()
+
+    download_youtube(args.url, args.output_dir)
